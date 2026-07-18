43ef7d09c6ffad3dd90a3366f6332025f64d21d8
Gauransh-Sharma-2005 <igauransh2005@gmail.com>
2026-07-19 00:10:33 +0530

    Different operations in JS

---
+++ Section-1-Javascript/part-1/operations.js
@@ -0,0 +1,27 @@
+// referencing through variables
+let score = 102;
+let bonus = 25;
+
+let totalScore = score + bonus;
+
+
+
+let addition = 4 + 5; // Direct Referencing
+let subtract = 9 - 3;
+let mult = 3 * 4;
+let division = 4 / 2;
+let remainder = 9 % 2;
+let exponent = 2 ** 3;
+
+let increment = 110;
+increment += 1; // or increment++
+
+let decrement = 50;
+decrement -= 1; // or decrement--
+
+let num1 = 3;
+let num2 = 3;
+let num3 = 6;
+
+console.log(num1 == num2); // returns a boolean result
+console.log(num1 != num2);
