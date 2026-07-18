b153109a6b8b430dc5935d180edea518d0689e49
Gauransh-Sharma-2005 <igauransh2005@gmail.com>
2026-07-19 00:08:40 +0530

    difference in variable declaration

---
+++ Section-1-Javascript/part-1/changes.js
@@ -0,0 +1,16 @@
+let gameName = "spiderman"
+console.log(gameName);
+
+gameName = "John Wick";
+console.log(gameName);
+
+
+var userName = "Ansh";
+console.log(userName);
+
+userName = "Gauransh";
+console.log(userName);
+
+// const ensures more reliability
+const musicName = "Dhadak";
+musicName = "Malhari"; // TypeError: Assignment to constant variable.
