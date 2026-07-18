72710c88ce4f5b7eb092a5da2017917fc2ce2dd2
Gauransh-Sharma-2005 <igauransh2005@gmail.com>
2026-07-19 00:11:02 +0530

    different ways to print in JS

---
+++ Section-1-Javascript/part-1/printing.js
@@ -0,0 +1,5 @@
+console.log("Hello"); // Gives a new line by a inbuilt function
+process.stdout.write("Chai"); // does not give a newline
+console.log();
+
+console.table({city: "Jaipur" }); // Returns the UI of a table
