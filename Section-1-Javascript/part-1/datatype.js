30e2498aebc7e2426a8dea16d8ee6464ffa9992b
Gauransh-Sharma-2005 <igauransh2005@gmail.com>
2026-07-19 00:09:02 +0530

    Types of Datatypes in JS

---
+++ Section-1-Javascript/part-1/datatype.js
@@ -0,0 +1,29 @@
+/*
+
+String
+Number
+Boolean -> True or False
+Bigint -> used for scientific calculations
+
+undefined -> there is no definition
+null -> empty
+
+object
+
+Symbol -> used to make something unique
+
+*/
+
+var score = 102; // old way or deprecated
+
+let score1 = 102; // New way
+let name = "Gauransh";
+let isLoggedIn = true;
+
+//object
+let teaTypes = ["Ginger", "Lemon", "Orange", "Oolong"]; // Array or collection
+let user = {firstname: "Ansh", lastname: "Sharma"}; // key-value pair
+
+let getScore = score;
+
+console.log(getScore);
