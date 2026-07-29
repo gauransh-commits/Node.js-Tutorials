81af74715c88bacaad2a7ecec8146127735a9bef
Gauransh-Sharma-2005 <igauransh2005@gmail.com>
2026-07-29 21:34:29 +0530

    Difference between Window and Browser Objects

---
+++ Section-2-Nodejs/part-1/script.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+
+alert("This is an alert"); // this will throw an error when executed outside of the browser engine
+// The alert belongs to the window object of the browser and cannot be executed outside of the browser
+
+
