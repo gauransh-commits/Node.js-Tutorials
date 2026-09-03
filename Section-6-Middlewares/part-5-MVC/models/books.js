73bdfd21697d17cc2040c0b4d04491a5e38e4eef
Gauransh-Sharma-2005 <igauransh2005@gmail.com>
2026-09-03 22:26:16 +0530

    In memory db which manages application data

---
+++ Section-6-Middlewares/part-5-MVC/models/books.js
@@ -0,0 +1,14 @@
+// In memory DB models
+
+exports.BOOKS = [
+    {
+        id:1,
+        title: "Book One",
+        author: "Author One",
+    },
+    {
+        id:2,
+        title: "Book Two",
+        author: "Author Two",
+    }
+]
